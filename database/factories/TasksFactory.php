62cdf7804cf900dc774dd94079131cdb88807f0f
p-chan5150 <kletuscorps3@gmail.com>
2026-03-31 19:49:20 +0300

    we bawl

--- database/factories/TasksFactory.php
+++ database/factories/TasksFactory.php
@@ -9,10 +9,12 @@ class TasksFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => 'Complete task #' . rand(100, 999),
-            'due_date' => \fake()->dateTimeBetween('now', '+30 days'),
-            'priority' => \fake()->randomElement(['high', 'low', 'medium']),
-            'status'   => \fake()->randomElement(['pending', 'in_progress', 'done']),
+
+            // faker doesnt work on laravel 12 on railway but for some reason works on my 13.2.0  spent 3 hours on this I want to go to sleep
+            'title'     => 'Complete task #' . rand(100, 999),
+            'due_date'  => now()->addDays(rand(1, 30))->format('Y-m-d'),
+            'priority'  => ['low', 'medium', 'high'][rand(0, 2)],
+            'status'    => ['pending', 'in_progress', 'done'][rand(0, 2)],
         ];
     }
 }
