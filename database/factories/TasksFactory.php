87f55219fb9ad7a0f6d0d16ea0bf7d733522634c
p-chan5150 <kletuscorps3@gmail.com>
2026-03-31 19:33:26 +0300

    another one

--- database/factories/TasksFactory.php
+++ database/factories/TasksFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Tasks;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TasksFactory extends Factory
@@ -10,10 +9,10 @@ class TasksFactory extends Factory
     public function definition(): array
     {
         return [
-            'title'    => fake()->sentence(4),
-            'due_date' => fake()->dateTimeBetween('now', '+30 days'),
-            'priority' => fake()->randomElement(['high', 'low', 'medium']),
-            'status'   => fake()->randomElement(['pending', 'in_progress', 'done']),
+            'title'    => \fake()->sentence(4),
+            'due_date' => \fake()->dateTimeBetween('now', '+30 days'),
+            'priority' => \fake()->randomElement(['high', 'low', 'medium']),
+            'status'   => \fake()->randomElement(['pending', 'in_progress', 'done']),
         ];
     }
 }
