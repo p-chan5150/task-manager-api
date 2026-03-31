cb12504cad2d0b0e3d4c38482a5f317ef6ad9da2
p-chan5150 <kletuscorps3@gmail.com>
2026-03-31 19:28:41 +0300

    yet another faker fix

--- database/factories/TasksFactory.php
+++ database/factories/TasksFactory.php
@@ -5,27 +5,15 @@ namespace Database\Factories;
 use App\Models\Tasks;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-
-/**
- * @extends Factory<Tasks>
- */
 class TasksFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
-
-        // TODO: make faker source from enums
-        $faker = \Faker\Factory::create();
         return [
-            'title'    => $faker->sentence(4),
-            'due_date' => $faker->dateTimeBetween('now', '+30 days'),
-            'priority' => $faker->randomElement(['high', 'low', 'medium']),
-            'status'   => $faker->randomElement(['pending', 'in_progress', 'done']),
+            'title'    => fake()->sentence(4),
+            'due_date' => fake()->dateTimeBetween('now', '+30 days'),
+            'priority' => fake()->randomElement(['high', 'low', 'medium']),
+            'status'   => fake()->randomElement(['pending', 'in_progress', 'done']),
         ];
     }
 }
